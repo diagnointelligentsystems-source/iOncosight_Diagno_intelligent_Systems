f2de2f2ac17088828bf8459221b28cab7399a129
diagnointelligentsystems-source <diagnointelligentsystems@gmail.com>
2025-09-23 16:26:23 +0530

    Update DL_model_ENB3andIncV3_code_and_seg_main.py

--- DL_model_ENB3andIncV3_code_and_seg_main.py
+++ DL_model_ENB3andIncV3_code_and_seg_main.py
@@ -356,7 +356,7 @@ def full_code(image_path,eff_model,inc_model,rf_chi2_ens,xgb_chi2_ens,rf_mi_ens,
         img_samp = cv2.imread(image_path)
         print('image_path :',image_path)
         print("img_samp shape:", img_samp.shape) 
-        results = model(image_path, conf=0.2, iou=0.5, imgsz=1024, device="cpu")
+        results = model(image_path, conf=0.5, iou=0.5, imgsz=512, device="cpu")
         print('ex 1_2_3')
         result = results[0]
         print('ex 1_3')
@@ -973,6 +973,7 @@ def full_code(image_path,eff_model,inc_model,rf_chi2_ens,xgb_chi2_ens,rf_mi_ens,
     ################3
 
     return imp_result,max_confidence_ML
+
 
 
 
