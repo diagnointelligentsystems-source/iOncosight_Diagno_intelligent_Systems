6c4303876b7d5773078c4810dfb5d095c1994a28
diagnointelligentsystems-source <diagnointelligentsystems@gmail.com>
2025-09-26 17:48:58 +0530

    Update seg_code_v11.py

--- seg_code_v11.py
+++ seg_code_v11.py
@@ -1,5 +1,5 @@
     ########################## segmentation model
-def seg_code(yolov11,image_path,predicted_proba_DL,predicted_value,sel_ens_M1, sel_ens_M2, sel_ens_M3, scaled_ens_M1, scaled_ens_M2, scaled_ens_M3, ens_MCN):
+def seg_code(img_p,yolov11,image_path,predicted_proba_DL,predicted_value,sel_ens_M1, sel_ens_M2, sel_ens_M3, scaled_ens_M1, scaled_ens_M2, scaled_ens_M3, ens_MCN):
     import os
     import sys
     import cv2
@@ -650,5 +650,6 @@ def seg_code(yolov11,image_path,predicted_proba_DL,predicted_value,sel_ens_M1, s
     plt.close('all')
 
     return imp_result, max_confidence_ML
+
 
 
