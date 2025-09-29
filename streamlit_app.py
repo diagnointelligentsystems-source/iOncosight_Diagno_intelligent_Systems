4c9d10ecb213111f6d6a262378a5cfbc7adc4386
diagnointelligentsystems-source <diagnointelligentsystems@gmail.com>
2025-09-29 17:04:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1145,16 +1145,15 @@ with col2:
         img_resized.save(output_path)
         try:
           # your main app code
-            import DL_model_ENB3andIncV3_code_and_seg_main
-            from DL_model_ENB3andIncV3_code_and_seg_main import full_code
+          import DL_model_ENB3andIncV3_code_and_seg_main
+          from DL_model_ENB3andIncV3_code_and_seg_main import full_code
                
-            imp_result,max_confidence_ML = full_code(output_path, eff_model, inc_model,rf_chi2_ens,xgb_chi2_ens,rf_mi_ens,ens_scaler_rf_chi2,ens_scaler_xgb_chi2,ens_scaler_rf_mi,
+          imp_result,max_confidence_ML = full_code(output_path, eff_model, inc_model,rf_chi2_ens,xgb_chi2_ens,rf_mi_ens,ens_scaler_rf_chi2,ens_scaler_xgb_chi2,ens_scaler_rf_mi,
                                                        st_ens_LC_NR)
-
-
         except Exception as e:
-            st.error(f"⚠️ The app crashed with error: {e}")
-            st.stop()
+          st.error(f"⚠️ App crashed with error: {e}. Retrying in 5s...")
+          time.sleep(5)
+          st.experimental_rerun()
         print('final_impression', imp_result, flush=True)
         #print('output image path :', imp_image_out)
         imp=imp_result
